8ec619adf1d6c2993400188683a2c71cc84b6d05
Peter Terrill <peter.terrill@gmail.com>
2013-01-06 17:25:51 -0800

    adding the rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingSite::Application.config.secret_token = '15dd7c2712ea91ef16192345643de54c4f52baa5f981e90e195294dda45590a0bb6fb4bb99ef81dd7dd93359480079826a1d92fa3b05d4ce0d4e9018154f79e8'
